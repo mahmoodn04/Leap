8e28969b4b0cbfdef581c063c30ba0e55865cdc0
mahmoodn04 <40336250+mahmoodn04@users.noreply.github.com>
2024-02-26 18:49:55 +0100

    Refactoring

--- Leap/Leap.cs
+++ Leap/Leap.cs
@@ -3,8 +3,6 @@ static void main()
 {}
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        return (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)? true:false;
-    }
+    public static bool IsLeapYear(int year) => (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)? true:false;
+    
 }
